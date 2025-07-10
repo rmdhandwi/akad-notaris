d2a1ce89130a87f5ad45aca7c4d7409f8470de99
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-10 22:13:53 +0800

    config : primevue - import from primevue - define custom preset using aura and custom colors - use : Primevue, ToastService, ConfirmationService

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,36 @@
 import '../css/app.css';
 import './bootstrap';
 
+// primevue
+import PrimeVue from 'primevue/config'
+import Aura from "@primeuix/themes/aura";
+import ToastService from "primevue/toastservice";
+import ConfirmationService from "primevue/confirmationservice";
+import { definePreset } from "@primeuix/themes";
+
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const MyPreset = definePreset(Aura, {
+    // tailwind-blue
+    semantic: {
+        primary: {
+            50: "#eff6ff",
+            100: "#dbeafe",
+            200: "#bfdbfe",
+            300: "#93c5fd",
+            400: "#60a5fa",
+            500: "#3b82f6",
+            600: "#2563eb",
+            700: "#1d4ed8",
+            800: "#1e40af",
+            900: "#1e3a8a",
+            950: "#172554",
+        },
+    },
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,15 +39,25 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob("./Pages/**/*.vue")
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: MyPreset,
+                    options: {
+                        darkModeSelector: false,
+                    },
+                },
+            })
+            .use(ToastService)
+            .use(ConfirmationService)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#3b82f6",
     },
 });
