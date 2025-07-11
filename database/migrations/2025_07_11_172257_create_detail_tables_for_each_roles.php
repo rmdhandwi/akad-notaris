5310a7ae535501e255d8a158ca6d7c7a608661be
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 01:28:18 +0800

    feat : define tables scheme

--- database/migrations/2025_07_11_172257_create_detail_tables_for_each_roles.php
+++ database/migrations/2025_07_11_172257_create_detail_tables_for_each_roles.php
@@ -6,5 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-   
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staf_details', function (Blueprint $table) {
+            $table->id('staf_id');
+            $table->string('nik_staf');
+            $table->string('nama_staf');
+            $table->timestamps();
+        });
+        Schema::create('notaris_details', function (Blueprint $table) {
+            $table->id('notaris_id');
+            $table->string('nomor_jabatan_notaris');
+            $table->string('nama_notaris');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staf_details');
+        Schema::dropIfExists('notaris_details');
+    }
 };
