e9e416677fb4112c3b908bac9e70370f126bf5e8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-11 21:05:38 +0800

    feat : show notification - import from vue : onMounted, watch - import from inertiajs/vue3 : usePage - import from composables : useNotification - define variables : page, showToast - catch notifciation data in onMounted - watch page props for showing notification for reactivity

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,7 +1,35 @@
 <script setup>
+// import core api
+import { onMounted, watch } from 'vue'
+import { usePage } from '@inertiajs/vue3'
+
+// import composables/stores
+import { useNotification } from '@/Composables/useNotification'
+
+// lifecycle hooks
+onMounted(() =>
+{
+    // catch notification data
+    const notification = page.props.flash
+    if (notification) {
+        showToast(notification.notif_message, notification.notif_status, notification.notif_duration)
+    }
+})
+
+// variables, functions
+const page = usePage()
+
+const { showToast } = useNotification()
+
+watch(() => page.props.flash, (newNotification) => {
+  if (newNotification) {
+    showToast(newNotification.notif_message, newNotification.notif_status, newNotification.notif_duration)
+  }
+})
 </script>
 
 <template>
+    <Toast position="bottom-right" group="br"/>
     <div class="flex justify-center items-center min-h-screen bg-slate-100">
         <div class="flex flex-col">
             <slot />
