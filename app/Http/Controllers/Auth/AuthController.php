e16b8c6c4a86c9cef330c8454d713f94692c51bb
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 02:38:47 +0800

    feat : implement login feature - implement service : AuthService - create function : login, logout - handle users login and logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,14 +3,49 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Services\AuthService;
+use App\Services\RedirectWithNotification;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 
 class AuthController extends Controller
 {
-    //
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    // return login page
     public function index()
     {
         return Inertia::render('Auth/Login');
+    }
+
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $this->authService->login($validated);
+
+        return RedirectWithNotification::flash(
+            true,
+            'Berhasil Login',
+            'Gagal login',
+        );
+    }
+
+    public function logout(): RedirectResponse
+    {
+        $this->authService->logout();
+
+        // return redirect()->route('login')->with('message', 'Logout berhasil.');
+        return RedirectWithNotification::flash(
+            true,
+            'Berhasil logout',
+            'Gagal logout',
+        );
     }
 }
