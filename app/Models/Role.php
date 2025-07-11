59568ed28d404743be5de4ae6a30255535c3b143
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 01:55:19 +0800

    fix : minor fix - define table custom primaryKey column - define table fillable - define table relation : roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    // define table primary key column
+    protected $primaryKey = 'role_id';
+
+    // define table fillable column
+    protected $fillable = [
+        'role_name',
+    ];
+
+    // define table relationship
+    public function users()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
 }
