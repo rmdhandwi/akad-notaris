94ed52a93ed6641801e09fc092e22757eb159ad8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 02:35:44 +0800

    feat : add post route for user login

--- routes/auth.php
+++ routes/auth.php
@@ -7,6 +7,8 @@ Route::middleware('guest')->group(function () {
     // login routes
     Route::get('/', [AuthController::class, 'index'])->name('user.login.index');
 
+    Route::post('/', [AuthController::class, 'login'])->name('user.login');
+
 });
 
 Route::middleware('auth')->group(function () {
