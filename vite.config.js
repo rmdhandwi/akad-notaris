39715db0cfec4c42e5d803cf47f4a308bc1b18c3
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-11 19:00:54 +0800

    project : configure @primevue/auto-import-resolver

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
                 },
             },
         }),
+        Components({
+            resolvers: [PrimeVueResolver()],
+        }),
     ],
     resolve: {
         alias: {
