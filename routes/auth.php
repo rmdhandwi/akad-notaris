87e8dc1cc86cdfe955caef341c8bb630b460b06b
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-11 19:12:07 +0800

    project : add route for login page

--- routes/auth.php
+++ routes/auth.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-   
+    // login routes
+    Route::get('/', [AuthController::class, 'index'])->name('user.login.index');
+
 });
 
 Route::middleware('auth')->group(function () {
