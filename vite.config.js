0f4c2f9859a475ee3718776fd9f0b19a00a60d95
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-11 19:32:57 +0800

    project : import resolve, Components, PrimeVueResolver

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 export default defineConfig({
     plugins: [
