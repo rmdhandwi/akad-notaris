c22fdfff242db2b425e9b43dcff13baa7a303e5c
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 20:36:20 +0800

    feat : create method : dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AdminController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        return Inertia::render('Admin/Dashboard');
+    }
 }
