7c1e095e94553d00da166bd273db15d3ba002132
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 19:27:32 +0800

    feat : register RoleNameMiddleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -3,22 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleNameMiddleware;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
-        //
+        // Daftarkan alias middleware
+        Route::aliasMiddleware('roleName', RoleNameMiddleware::class);
     }
 }
