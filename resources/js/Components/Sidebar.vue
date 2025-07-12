445fe18740eef75915648e923a49bb950a476021
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 20:55:27 +0800

    fix : fix sidebar width

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -14,9 +14,9 @@ const menus = computed(() =>
 </script>
 
 <template>
-    <div class="transition-all ease-in-out duration-[450ms] rounded-lg h-[99vh] p-2 flex flex-col bg-gradient-to-b from-blue-500 to-blue-400 gap-[2rem] justify-between fixed">
+    <div class="transition-all ease-in-out duration-[450ms] rounded-lg w-[180px] h-[99vh] p-2 flex flex-col bg-gradient-to-b from-blue-500 to-blue-400 gap-[2rem] justify-between fixed">
         <div class="flex flex-col gap-4 text-lg items-center">
-            <Button v-for="menu in menus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="w-full p-1 flex items-center" :class="{'bg-slate-100 text-sky-500 rounded' : route().current(menu.route),'text-slate-50' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
+            <Button v-for="menu in menus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="w-full p-1 flex gap-x-2 items-center" :class="{'bg-slate-100 text-sky-500 rounded' : route().current(menu.route),'text-slate-50' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
         </div>
     </div>
 </template>
