4ac7753128f0252816a0330e7a7296cdaa4944c7
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 20:53:27 +0800

    feat : add toNamedRoute method

--- app/Services/RedirectWithNotification.php
+++ app/Services/RedirectWithNotification.php
@@ -4,9 +4,18 @@ namespace App\Services;
 
 class RedirectWithNotification
 {
-    public static function flash(bool $action, string $successMessage, string $errorMessage, int $duration = 3000)
+    public static function back(bool $action, string $successMessage, string $errorMessage, int $duration = 3000)
     {
         return redirect()->back()->with([
+            'notif_status' => $action ? 'success' : 'error',
+            'notif_message' => $action ? $successMessage : $errorMessage,
+            'notif_duration' => $duration,
+        ]);
+    }
+
+    public static function toNamedRoute(string $namedRoute, bool $action, string $successMessage, string $errorMessage, int $duration = 3000)
+    {
+        return redirect()->route($namedRoute)->with([
             'notif_status' => $action ? 'success' : 'error',
             'notif_message' => $action ? $successMessage : $errorMessage,
             'notif_duration' => $duration,
