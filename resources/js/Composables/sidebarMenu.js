6fad26dd9015b701074d44bf4fbe92359bb28e8d
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-12 20:37:35 +0800

    feat : define adminMenu

---
+++ resources/js/Composables/sidebarMenu.js
@@ -0,0 +1,6 @@
+export const adminMenu = [
+    { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
+    { label: "Data Layanan", icon: "pi pi-briefcase", route: "admin.layanan.index"},
+    { label: "Data Staf", icon: "pi pi-user", route: "admin.staf.index"},
+    { label: "Data Notaris", icon: "pi pi-user", route: "admin.notaris.index"},
+]
