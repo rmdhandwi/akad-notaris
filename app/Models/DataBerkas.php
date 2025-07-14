3820d5279bdb663c19a6297de5b73ce1cf807716
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 17:50:11 +0800

    project : define table columns and relation

--- app/Models/DataBerkas.php
+++ app/Models/DataBerkas.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DataBerkas extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'data_berkas';
+    protected $primaryKey = 'id_berkas';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_jenis',
+        'nama_berkas',
+    ];
+
+    public function jenisLayanan()
+    {
+        return $this->belongsTo(JenisLayanan::class, 'id_jenis', 'id_jenis');
+    }
 }
