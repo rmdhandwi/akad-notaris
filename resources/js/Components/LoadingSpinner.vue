8813acdd5ba39f14061642b7176da967113a9a2d
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 20:20:34 +0800

    feat : create loading animation

---
+++ resources/js/Components/LoadingSpinner.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex place-content-center w-full py-[4rem]">
+        <i class="pi pi-spin pi-spinner text-blue-500" style="font-size: 2rem"></i>
+    </div>
+</template>
