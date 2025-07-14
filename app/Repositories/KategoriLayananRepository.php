db882fb98d1c6ae80e70a0f7568245e14882242b
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 21:50:01 +0800

    feat : handle create data

--- app/Repositories/KategoriLayananRepository.php
+++ app/Repositories/KategoriLayananRepository.php
@@ -2,11 +2,15 @@
 
 namespace App\Repositories;
 
+use App\Models\KategoriLayanan;
 use App\Repositories\Interfaces\KategoriLayananRepositoryInterface;
 
 class KategoriLayananRepository implements KategoriLayananRepositoryInterface
 {
-    public function create(array $data) {}
+    public function create(array $data) : bool {
+        $store = KategoriLayanan::create($data);
+        return (bool) $store;
+    }
     public function update($id, array $data) {}
     public function delete($id) {}
 }
