f8857fbd2b4299030aef79771dfb35a6a1e29b23
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 19:51:02 +0800

    feat : add kategori layanan index route for admin

--- routes/auth.php
+++ routes/auth.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Models\KategoriLayanan;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -20,4 +21,6 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'roleName:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+    Route::get('/admin/layanan/kategori', [KategoriLayanan::class, 'index'])->name('admin.layanan.kategori.index');
 });
