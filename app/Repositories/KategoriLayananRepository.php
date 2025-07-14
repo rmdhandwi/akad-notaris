3b5b6fae72821410d4f90634d93c7b2aa756d1e8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 20:03:20 +0800

    feat : define method

---
+++ app/Repositories/KategoriLayananRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\KategoriLayananRepositoryInterface;
+
+class KategoriLayananRepository implements KategoriLayananRepositoryInterface
+{
+    public function create(array $data) {}
+    public function update($id, array $data) {}
+    public function delete($id) {}
+}
