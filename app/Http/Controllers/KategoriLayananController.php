8a7f78e216b8395141da59a258532ee43fd5f606
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 21:51:13 +0800

    feat : handle create data - import related repository - bind repository to controller - define store method - handle store data with validation and repository

--- app/Http/Controllers/KategoriLayananController.php
+++ app/Http/Controllers/KategoriLayananController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\KategoriLayananRequest;
 use App\Models\KategoriLayanan;
-use Illuminate\Http\Request;
+use App\Repositories\Interfaces\KategoriLayananRepositoryInterface;
+use App\Services\RedirectWithNotification;
 use Inertia\Inertia;
 
 class KategoriLayananController extends Controller
 {
+    // repository
+    protected $repository;
+
+    public function __construct(KategoriLayananRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
         $data = KategoriLayanan::select('id_kategori','nama_kategori','deskripsi_kategori')->get();
@@ -15,5 +25,18 @@ class KategoriLayananController extends Controller
         return Inertia::render('Admin/KategoriLayanan/Index',[
             'data' => $data,
         ]);
+    }
+
+    public function store(KategoriLayananRequest $req)
+    {
+        $data = $req->validated();
+
+        $create = $this->repository->create($data);
+
+        return RedirectWithNotification::back(
+            $create,
+            'Berhasil menambahkan '.$req['nama_kategori'],
+            'Gagal menambahkan '.$req['nama_kategori'],
+        );
     }
 }
