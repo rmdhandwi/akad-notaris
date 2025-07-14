948bca76b5a51963def8c51af2e25e0f95ad62a9
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 19:51:26 +0800

    feat : add index method

--- app/Http/Controllers/KategoriLayananController.php
+++ app/Http/Controllers/KategoriLayananController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriLayanan;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class KategoriLayananController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = KategoriLayanan::select('id_kategori','nama_kategori','deksripsi_kategori')->get();
+
+        return Inertia::render('Admin/KategoriLayanan/Index',[
+            'data' => $data,
+        ]);
+    }
 }
