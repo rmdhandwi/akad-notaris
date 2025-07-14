4a04c279e839e91b844dc6eb66fb39cc058ba6da
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 17:50:03 +0800

    project : define table columns and relation

--- app/Models/KategoriLayanan.php
+++ app/Models/KategoriLayanan.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class KategoriLayanan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'kategori_layanan';
+    protected $primaryKey = 'id_kategori';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama_kategori',
+        'deskripsi',
+    ];
+
+    public function jenisLayanan()
+    {
+        return $this->hasMany(JenisLayanan::class, 'id_kategori', 'id_kategori');
+    }
 }
