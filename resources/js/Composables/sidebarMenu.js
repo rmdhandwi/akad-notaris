a9d28e84c8c4d9631434414682803c36d008abe6
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 19:04:28 +0800

    feat : add new menu for admin

--- resources/js/Composables/sidebarMenu.js
+++ resources/js/Composables/sidebarMenu.js
@@ -1,6 +1,8 @@
 export const adminMenu = [
     { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
-    { label: "Data Layanan", icon: "pi pi-briefcase", route: "admin.layanan.index"},
+    { label: "Kategori Layanan", icon: "pi pi-briefcase", route: "admin.kategori_layanan.index"},
+    { label: "Jenis Layanan", icon: "pi pi-briefcase", route: "admin.jenis_layanan.index"},
+    { label: "Data Berkas", icon: "pi pi-book", route: "admin.berkas.index"},
     { label: "Data Staf", icon: "pi pi-user", route: "admin.staf.index"},
     { label: "Data Notaris", icon: "pi pi-user", route: "admin.notaris.index"},
 ]
