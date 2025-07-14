c59d9700aab2034b2a00e8e77528e446cf451744
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-14 20:39:26 +0800

    feat : define layout and handle switch components

--- resources/js/Pages/Admin/KategoriLayanan/Index.vue
+++ resources/js/Pages/Admin/KategoriLayanan/Index.vue
@@ -1,9 +1,83 @@
 <script setup>
+// import core api
+import { computed, defineAsyncComponent, ref } from 'vue'
+
+// import store / composables
+import { useNotification } from '@/Composables/useNotification'
+
+// import layout, components
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import LoadingSpinner from '@/Components/LoadingSpinner.vue'
+
+// variables, functions
+const props = defineProps({
+    data : Object
+})
+
+const { showToast } = useNotification()
+
+const pageTitle = ref('Kategori Layanan')
+
+const currentTab = ref('List')
+
+
+const switchComponents = (component,title) =>
+{
+    currentTab.value = component
+    pageTitle.value = title
+}
+
+// async component
+const loadComponent = componentName =>
+{
+    return  defineAsyncComponent({
+        loader : () => import(`./${componentName}.vue`),
+        loadingComponent : LoadingSpinner,
+        delay : 200,
+        timeout: 3000
+    })
+}
+
+const currentComponent = computed(() => {
+  return loadComponent(currentTab.value)
+})
+
+const componentProps = computed(() => {
+    switch (currentTab.value) {
+
+        case 'List':
+        return {
+            data: props.data?.map((p, i) => ({ nomor: i + 1, ...p })),
+        };
+
+        default:
+        return {};
+    }
+})
+
+const refreshPage = () =>
+{
+    showToast()
+    switchComponents('List','Daftar Kategori')
+}
+
+
 </script>
 
 <template>
-    <div>
-    </div>
+    <AuthenticatedLayout :pageTitle="pageTitle">
+        <template #pageContent>
+            <!-- tabs -->
+            <div class="flex gap-x-4">
+                <Button @click="switchComponents('List','Daftar Kategori')" label="Daftar Kategori" :severity="currentTab==='List'?'primary':'secondary'" icon="pi pi-list"/>
+                <Button @click="switchComponents('Form','Tambah Kategori')" label="Tambah Kategori" :severity="currentTab==='List'?'secondary':'primary'" icon="pi pi-plus"/>
+            </div>
+            <!-- components -->
+            <div class="flex flex-col mt-4">
+                <component :is="currentComponent" v-bind="componentProps" @refreshPage="refreshPage()"/>
+            </div>
+        </template>
+    </AuthenticatedLayout>
 </template>
 
 <style scoped>
