333fc6cf5cfd03334be302611d91b4f520079e50
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-15 21:04:16 +0800

    feat : minor fix

--- resources/js/Pages/Admin/JenisLayanan/List.vue
+++ resources/js/Pages/Admin/JenisLayanan/List.vue
@@ -7,12 +7,13 @@ import { FilterMatchMode } from '@primevue/core/api'
 // lifecycle hooks
 onBeforeMount(() =>
 {
-    dataClone.value = props.data
+    dataJenisClone.value = props.dataJenis
 })
 
 // variables, functions
 const props = defineProps({
-    data : Object
+    dataKtg : Object,
+    dataJenis : Object
 })
 
 const emit = defineEmits(['refreshPage', 'editData'])
@@ -21,7 +22,7 @@ const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 })
 
-const dataClone = ref([])
+const dataJenisClone = ref([])
 
 const editData = dataId =>
 {
@@ -32,7 +33,7 @@ const editData = dataId =>
 <template>
     <div class="flex flex-col">
         <!-- dataTable -->
-        <DataTable :value="dataClone" dataKey="index" class="shadow border border-blue-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable>
+        <DataTable :value="dataJenisClone" dataKey="index" class="shadow border border-blue-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable>
             <template #header>
                 <div class="flex flex-col gap-y-2">
                     <!-- Basic Filter -->
@@ -47,27 +48,28 @@ const editData = dataId =>
                 </div>
             </template>
             <template #footer>
-                <span>Jumlah Kategori ({{ dataClone.length }})</span>
+                <span>Jumlah Jenis Layanan ({{ dataJenisClone.length }})</span>
             </template>
             <template #loading>
                 <span class="flex justify-center">Sedang Memuat...</span>
             </template>
             <template #empty>
-                <span class="flex justify-center">Tidak Ada Kategori</span>
+                <span class="flex justify-center">Tidak Ada Jenis Layanan</span>
             </template>
             <Column header="No" field="nomor"/>
-            <Column header="Nama" field="nama_kategori"/>
-            <Column header="Deskripsi" field="deskripsi_kategori">
+            <Column header="Kategori Layanan" field="kategori_layanan.nama_kategori"/>
+            <Column header="Nama" field="nama_jenis"/>
+            <Column header="Deskripsi" field="deskripsi_jenis">
                 <template #body="{data}">
-                    <span :class="{'text-gray-400' : !data.deskripsi_kategori}">
-                        {{ data.deskripsi_kategori ? data.deskripsi_kategori : 'Tidak ada deskripsi' }}
+                    <span :class="{'text-gray-400' : !data.deskripsi_jenis}">
+                        {{ data.deskripsi_jenis ? data.deskripsi_jenis : 'Tidak ada deskripsi' }}
                     </span>
                 </template>
             </Column>
             <Column header="Action" frozen align-frozen="right" class="w-1 whitespace-nowrap">
                 <template #body="{data}">
                     <div class="flex items-center gap-2">
-                        <Button @click="editData(data.id_kategori)" severity="info" size="small" icon="pi pi-pen-to-square"/>
+                        <Button @click="editData(data.id_jenis)" severity="info" size="small" icon="pi pi-pen-to-square"/>
                     </div>
                 </template>
             </Column>
