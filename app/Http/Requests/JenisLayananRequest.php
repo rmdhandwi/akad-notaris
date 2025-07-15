b8e2c4e4cdf2fb1bcb297ee8b4ddc5d7b17f33e8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-15 21:04:48 +0800

    feat : define validation attributes, rules, messages

--- app/Http/Requests/JenisLayananRequest.php
+++ app/Http/Requests/JenisLayananRequest.php
@@ -3,26 +3,55 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class JenisLayananRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function attributes(): array
     {
         return [
-            //
+            'id_kategori' => 'Kategori',
+            'nama_jenis' => 'Nama jenis',
+            'deskripsi_jenis' => 'Deskripsi jenis',
+        ];
+    }
+
+    public function rules(): array
+    {
+        $id_jenis = $this->input('id_jenis');
+
+        $isUpdate = !empty($id_jenis);
+
+        return [
+            'id_kategori' => $isUpdate ? ['required', 'exists:kategori_layanan,id_kategori'] : 'nullable',
+            'nama_jenis' => [
+                'required',
+                'string',
+                $isUpdate ?
+                    Rule::unique('jenis_layanan', 'nama_jenis')->ignore($id_jenis, 'id_jenis') :
+                    Rule::unique('jenis_layanan', 'nama_jenis'),
+            ],
+            'deskripsi_jenis' => [
+                'nullable',
+                'string',
+                $isUpdate ?
+                    Rule::unique('jenis_layanan', 'deskripsi_jenis')->ignore($id_jenis, 'id_jenis') :
+                    Rule::unique('jenis_layanan', 'deskripsi_jenis'),
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            '*.required' => ':attribute wajib diisi.',
+            '*.unique'   => ':attribute sudah terdaftar.',
+            '*.exist'   => ':attribute tidak terdaftar.',
         ];
     }
 }
