f7743ea3987f021262ddc4ad6aa455064be12d81
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-15 18:33:20 +0800

    feat : define methods

---
+++ app/Repositories/JenisLayananRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\JenisLayanan;
+use App\Repositories\Interfaces\JenisLayananRepositoryInterface;
+
+class JenisLayananRepository implements JenisLayananRepositoryInterface
+{
+    public function create(array $data): bool
+    {
+        $store = JenisLayanan::create($data);
+        return (bool) $store;
+    }
+    public function update($id, array $data) {}
+    public function delete($id) {}
+}
