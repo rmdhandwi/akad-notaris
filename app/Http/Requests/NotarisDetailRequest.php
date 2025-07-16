3c1aff41713d6e3a0c188e3c380bac1c22ebc65f
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 21:05:32 +0800

    feat : fix validation attributes, rules and messages

--- app/Http/Requests/NotarisDetailRequest.php
+++ app/Http/Requests/NotarisDetailRequest.php
@@ -16,42 +16,63 @@ class NotarisDetailRequest extends FormRequest
     {
         return [
             'user_id' => 'Notaris',
-            'nomor_jabatan_notaris' => 'Nomor jabatan notaris',
-            'nama_notaris' => 'Nama notaris',
+            'username' => 'Username',
+            'email' => 'Email',
+            'password' => 'Password',
+            'notaris_details.notaris_id' => 'ID notaris',
+            'notaris_details.nomor_jabatan_notaris' => 'Nomor Jabatan notaris',
+            'notaris_details.nama_notaris' => 'Nama notaris',
         ];
     }
 
     public function rules(): array
     {
-        $notaris_id = $this->input('notaris_id');
+        $notaris_id = $this->input('notaris_details.notaris_id');
 
         $isUpdate = !empty($notaris_id);
 
-        return [
-            'user_id' => ['required', 'exists:users,user_id'],
-            'nomor_jabatan_notaris' => [
+        $rules = [
+            'user_id' => $isUpdate ? ['required', 'exists:users,user_id'] : ['nullable'],
+            'notaris_details.notaris_id' => $isUpdate ? ['required', 'exists:notaris_details,notaris_id'] : ['nullable'],
+            'username' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'password' => [],
+            'notaris_details.nomor_jabatan_notaris' => [
                 'required',
                 'string',
                 $isUpdate ?
-                    Rule::unique('notaris_detail', 'nomor_jabatan_notaris')->ignore($notaris_id, 'notaris_id') :
-                    Rule::unique('notaris_detail', 'nomor_jabatan_notaris'),
+                    Rule::unique('notaris_details', 'nomor_jabatan_notaris')->ignore($notaris_id, 'notaris_id') :
+                    Rule::unique('notaris_details', 'nomor_jabatan_notaris'),
             ],
-            'nama_notaris' => [
+            'notaris_details.nama_notaris' => [
                 'required',
                 'string',
                 $isUpdate ?
-                    Rule::unique('notaris_detail', 'nama_notaris')->ignore($notaris_id, 'notaris_id') :
-                    Rule::unique('notaris_detail', 'nama_notaris'),
+                    Rule::unique('notaris_details', 'nama_notaris')->ignore($notaris_id, 'notaris_id') :
+                    Rule::unique('notaris_details', 'nama_notaris'),
             ],
         ];
+
+        if ($this->routeIs('admin.users.notaris.store')) {
+            $rules['password'] = ['required', 'string', 'min:6'];
+        } elseif ($this->routeIs('admin.users.notaris.update')) {
+            $rules['password'] = ['nullable', 'string', 'min:6'];
+        }
+
+        return $rules;
     }
 
     public function messages(): array
     {
         return [
+            '*.min' => ':attribute minimal :min digit.',
             '*.required' => ':attribute wajib diisi.',
             '*.unique'   => ':attribute sudah terdaftar.',
             '*.exists'   => ':attribute tidak terdaftar.',
+            'notaris_details.*.min' => ':attribute minimal :min digit.',
+            'notaris_details.*.required' => ':attribute wajib diisi.',
+            'notaris_details.*.unique'   => ':attribute sudah terdaftar.',
+            'notaris_details.*.exists'   => ':attribute tidak terdaftar.',
         ];
     }
 }
