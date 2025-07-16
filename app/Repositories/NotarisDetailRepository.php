26ec7c00921d88d6685b19dc545eab4a341009e3
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 21:11:47 +0800

    feat : implement each methods and logic business

--- app/Repositories/NotarisDetailRepository.php
+++ app/Repositories/NotarisDetailRepository.php
@@ -3,27 +3,87 @@
 namespace App\Repositories;
 
 use App\Models\NotarisDetail;
+use App\Models\User;
 use App\Repositories\Interfaces\NotarisDetailRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class NotarisDetailRepository implements NotarisDetailRepositoryInterface
 {
-    public function create(array $data): bool
+    public function createWithDetails(array $data): bool
     {
-        $store = NotarisDetail::create($data);
-        return (bool) $store;
+        DB::beginTransaction();
+
+        try {
+            $user = User::create([
+                'username' => $data['username'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'role_id' => 3,
+            ]);
+
+            if (isset($data['notaris_details'])) {
+                NotarisDetail::create([
+                    'user_id' => $user->user_id,
+                    'nomor_jabatan_notaris' => $data['notaris_details']['nomor_jabatan_notaris'],
+                    'nama_notaris' => $data['notaris_details']['nama_notaris'],
+                ]);
+            }
+
+            DB::commit();
+            return true;
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return false;
+        }
     }
 
     public function update($id, array $data)
     {
-        $getData = NotarisDetail::findOrFail($id);
+        DB::beginTransaction();
 
-        return $getData->update($data);
+        try {
+            $user = User::findOrFail($id);
+
+            $user->update([
+                'username' => $data['username'],
+                'email' => $data['email'],
+            ]);
+
+            // Update password hanya jika diisi
+            if (!empty($data['password'])) {
+                $user->update(['password' => Hash::make($data['password'])]);
+            }
+
+            if (isset($data['notaris_details'])) {
+                NotarisDetail::updateOrCreate(
+                    ['notaris_id' => $data['notaris_details']['notaris_id'] ?? null],
+                    [
+                        'user_id' => $id,
+                        'nomor_jabatan_notaris' => $data['notaris_details']['nomor_jabatan_notaris'],
+                        'nama_notaris' => $data['notaris_details']['nama_notaris'],
+                    ]
+                );
+            }
+
+            DB::commit();
+            return true;
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return false;
+        }
     }
 
     public function delete($id)
     {
-        $getData = NotarisDetail::findOrFail($id);
+        $getUser = User::findOrFail($id);
 
-        return $getData->delete();
+        $getStaf = NotarisDetail::where('user_id', $getUser->user_id);
+
+        if ($getUser->delete() && $getStaf->delete()) {
+            return true;
+        }
+
+        return false;
     }
 }
