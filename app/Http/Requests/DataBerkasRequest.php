0026bbe28e95d0e77d426c3d24b78fbed3aa544e
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 17:38:33 +0800

    feat : define validation attributes, rules, messages

--- app/Http/Requests/DataBerkasRequest.php
+++ app/Http/Requests/DataBerkasRequest.php
@@ -3,26 +3,47 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class DataBerkasRequest extends FormRequest
+class berkasLayananRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function attributes(): array
     {
         return [
-            //
+            'id_jenis' => 'Jenis layanan',
+            'nama_berkas' => 'Nama berkas',
+        ];
+    }
+
+    public function rules(): array
+    {
+        $id_berkas = $this->input('id_berkas');
+
+        $isUpdate = !empty($id_berkas);
+
+        return [
+            'id_jenis' => $isUpdate ? ['required', 'exists:jenis_layanan,id_jenis'] : 'nullable',
+            'nama_berkas' => [
+                'required',
+                'string',
+                $isUpdate ?
+                    Rule::unique('data_berkas', 'nama_berkas')->ignore($id_berkas, 'id_berkas') :
+                    Rule::unique('data_berkas', 'nama_berkas'),
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            '*.required' => ':attribute wajib diisi.',
+            '*.unique'   => ':attribute sudah terdaftar.',
+            '*.exist'   => ':attribute tidak terdaftar.',
         ];
     }
 }
