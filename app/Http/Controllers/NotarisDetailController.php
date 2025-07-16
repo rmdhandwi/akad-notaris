1673749fd2c1a0b1d7c6b851bd551ed3b8891989
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 21:37:49 +0800

    feat : fix data implement

--- app/Http/Controllers/NotarisDetailController.php
+++ app/Http/Controllers/NotarisDetailController.php
@@ -21,7 +21,7 @@ class NotarisDetailController extends Controller
     public function index()
     {
         $dataNotaris = User::where('role_id', 3)->select('user_id', 'username', 'email')->with([
-            'NotarisDetails:staf_id,user_id,nomor_jabatan_notaris,nama_notaris'
+            'NotarisDetails:notaris_id,user_id,nomor_jabatan_notaris,nama_notaris'
         ])->get();
 
         return Inertia::render('Admin/DataNotaris/Index', [
@@ -37,8 +37,8 @@ class NotarisDetailController extends Controller
 
         return RedirectWithNotification::back(
             $create,
-            'Berhasil menambahkan staf : ' . $req['notaris_details.nama_notaris'],
-            'Gagal menambahkan staf : ' . $req['notaris_details.nama_notaris'],
+            'Berhasil menambahkan notaris : ' . $req['notaris_details.nama_notaris'],
+            'Gagal menambahkan notaris : ' . $req['notaris_details.nama_notaris'],
         );
     }
 
@@ -50,8 +50,8 @@ class NotarisDetailController extends Controller
 
         return RedirectWithNotification::back(
             $update,
-            'Berhasil update staf : ' . $req['notaris_details.nama_notaris'],
-            'Gagal update staf : ' . $req['notaris_details.nama_notaris'],
+            'Berhasil update notaris : ' . $req['notaris_details.nama_notaris'],
+            'Gagal update notaris : ' . $req['notaris_details.nama_notaris'],
         );
     }
 
@@ -61,8 +61,8 @@ class NotarisDetailController extends Controller
 
         return RedirectWithNotification::back(
             $delete,
-            'Berhasil hapus staf : ' . $req['notaris_details.nama_notaris'],
-            'Gagal hapus staf : ' . $req['notaris_details.nama_notaris'],
+            'Berhasil hapus notaris : ' . $req['notaris_details.nama_notaris'],
+            'Gagal hapus notaris : ' . $req['notaris_details.nama_notaris'],
         );
     }
 }
