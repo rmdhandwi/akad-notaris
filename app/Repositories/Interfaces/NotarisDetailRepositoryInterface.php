0443734bd29a62bee52dc7032f0e447ba47216b3
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 21:11:17 +0800

    feat :  rename create method

--- app/Repositories/Interfaces/NotarisDetailRepositoryInterface.php
+++ app/Repositories/Interfaces/NotarisDetailRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Repositories\Interfaces;
 
 interface NotarisDetailRepositoryInterface
 {
-    public function create(array $data);
+    public function createWithDetails(array $data);
     public function update($id, array $data);
     public function delete($id);
 }
