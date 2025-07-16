1614f9de47181fe4e177de110cdb45308f757382
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 20:40:49 +0800

    feat : fix logic for create, update and delete method

--- app/Repositories/StafDetailRepository.php
+++ app/Repositories/StafDetailRepository.php
@@ -3,27 +3,88 @@
 namespace App\Repositories;
 
 use App\Models\StafDetail;
+use App\Models\User;
 use App\Repositories\Interfaces\StafDetailRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class StafDetailRepository implements StafDetailRepositoryInterface
 {
-    public function create(array $data): bool
+    public function createWithDetails(array $data): bool
     {
-        $store = StafDetail::create($data);
-        return (bool) $store;
+        DB::beginTransaction();
+
+        try {
+            $user = User::create([
+                'username' => $data['username'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'role_id' => 2,
+            ]);
+
+            if (isset($data['staf_details'])) {
+                StafDetail::create([
+                    'user_id' => $user->user_id,
+                    'nik_staf' => $data['staf_details']['nik_staf'],
+                    'nama_staf' => $data['staf_details']['nama_staf'],
+                ]);
+            }
+
+            DB::commit();
+            return true;
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return false;
+        }
     }
 
     public function update($id, array $data)
     {
-        $getData = StafDetail::findOrFail($id);
+        DB::beginTransaction();
 
-        return $getData->update($data);
+        try {
+            $user = User::findOrFail($id);
+
+            $user->update([
+                'username' => $data['username'],
+                'email' => $data['email'],
+            ]);
+
+            // Update password hanya jika diisi
+            if (!empty($data['password'])) {
+                $user->update(['password' => Hash::make($data['password'])]);
+            }
+
+            if (isset($data['staf_details'])) {
+                StafDetail::updateOrCreate(
+                    ['staf_id' => $data['staf_details']['staf_id'] ?? null],
+                    [
+                        'user_id' => $id,
+                        'nik_staf' => $data['staf_details']['nik_staf'],
+                        'nama_staf' => $data['staf_details']['nama_staf'],
+                    ]
+                );
+            }
+
+            DB::commit();
+            return true;
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return false;
+        }
     }
 
     public function delete($id)
     {
-        $getData = StafDetail::findOrFail($id);
+        $getUser = User::findOrFail($id);
 
-        return $getData->delete();
+        $getStaf = StafDetail::where('user_id', $getUser->user_id);
+
+        if($getUser->delete() && $getStaf->delete())
+        {
+            return true;
+        }
+
+        return false;
     }
 }
