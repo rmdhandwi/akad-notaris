c4b3b3b5e8f8136986dc77d1145d4890922899b3
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 18:16:41 +0800

    feat : create and implements dataBerkas

---
+++ resources/js/Pages/Admin/DataBerkas/Form.vue
@@ -0,0 +1,94 @@
+<script setup>
+// import core api
+import { watch } from 'vue'
+import { useForm } from '@inertiajs/vue3'
+import { useConfirm } from 'primevue'
+
+// import store / composables
+import { useNotification } from '@/Composables/useNotification'
+
+// lifecycle hooks
+
+// variables, functions
+const props = defineProps({
+    dataBerkas : Object,
+    dataJenis : Object,
+})
+
+const emit = defineEmits(['refreshPage'])
+
+const confirm = useConfirm()
+
+const { showToast } = useNotification()
+
+const form = useForm({
+    id_berkas : null,
+    id_jenis : null,
+    nama_berkas : null,
+})
+
+const submit = (Action, actionRoute) =>
+{
+    confirm.require({
+        message: `${Action} Berkas ${form.nama_berkas ?? ''}?`,
+        header: 'Peringatan',
+        icon: 'pi pi-exclamation-triangle',
+        rejectProps: {
+            label: 'Batal',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptProps: {
+            label: `${Action}`,
+            severity : actionRoute === 'delete' ? 'danger' : 'primary'
+        },
+        accept: () => {
+            form.post(route(`admin.berkas.${actionRoute}`), {
+                    onError : () => {
+                        showToast(
+                            'Terjadi kesalahan',
+                            'error',
+                            5000,
+                        )
+                    },
+                    onSuccess : () => {
+                        form.reset()
+                        form.clearErrors()
+                        emit('refreshPage')
+                    }
+            })
+        },
+    })
+}
+
+// reactivity
+watch(() => props.dataBerkas, (newData) => {
+  if (newData) {
+    Object.assign(form, newData)
+  } else {
+    form.reset()
+  }
+}, { immediate: true })
+
+</script>
+
+<template>
+    <form @submit.prevent class="flex flex-col gap-4 mt-4" autocomplete="off">
+        <div>
+            <FloatLabel variant="on">
+                <Select id="nama_jenis" v-model="form.id_jenis" placeholder="Jenis Layanan" :options="props.dataJenis" optionLabel="nama_jenis" optionValue="id_jenis" fluid/>
+            </FloatLabel>
+            <span class="text-red-500" v-if="form.errors.id_jenis"> {{ form.errors.id_jenis }} </span>
+        </div>
+        <div>
+            <FloatLabel variant="on">
+                <InputText id="nama_berkas" v-model="form.nama_berkas" fluid/>
+                <label for="nama_berkas">Nama Jenis</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="form.errors.nama_berkas"> {{ form.errors.nama_berkas }} </span>
+        </div>
+
+        <Button @click="submit(props.dataBerkas ? 'Update' : 'Simpan', props.dataBerkas ? 'update' : 'store')" :label="props.dataBerkas ? 'Update' : 'Simpan'" :disabled="form.processing"/>
+        <Button @click="submit('Hapus', 'delete')" label="Hapus" :disabled="form.processing" severity="danger" v-if="props.dataBerkas"/>
+    </form>
+</template>
