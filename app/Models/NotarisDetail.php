66724cf690b2a488679005dd349e13e4649caa75
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 18:53:59 +0800

    feat : define table and relation

--- app/Models/NotarisDetail.php
+++ app/Models/NotarisDetail.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class NotarisDetail extends Model
 {
-    //
+    use HasFactory;
+
+    protected
+        $table = 'notaris_details',
+        $primaryKey = 'notaris_id',
+        $fillable = [
+            'user_id',
+            'nik_notaris',
+            'nama_notaris',
+        ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
 }
