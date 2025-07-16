67382b5c64b316c282e30f1fb600359db2c3ae8b
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-16 18:53:50 +0800

    feat : define table and relation

--- app/Models/StafDetail.php
+++ app/Models/StafDetail.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StafDetail extends Model
 {
-    //
+    use HasFactory;
+
+    protected
+        $table = 'staf_details',
+        $primaryKey = 'staf_id',
+        $fillable = [
+            'user_id',
+            'nik_staf',
+            'nama_staf',
+        ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
 }
