e98e72786c26596bf961ab4e0b1986e55f1c75f6
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-17 17:53:04 +0800

    feat : define menu for notaris

--- resources/js/Composables/sidebarMenu.js
+++ resources/js/Composables/sidebarMenu.js
@@ -6,3 +6,8 @@ export const adminMenu = [
     { label: "Data Staf", icon: "pi pi-user", route: "admin.users.staf.index"},
     { label: "Data Notaris", icon: "pi pi-user", route: "admin.users.notaris.index"},
 ]
+
+export const notarisMenu = [
+    { label: "Dashboard", icon: "pi pi-home", route: "notaris.dashboard" },
+    { label: "Jadwal", icon: "pi pi-calendar", route: "notaris.layanan.jadwal.index"},
+]
