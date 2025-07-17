0d38881837ce7c6732b454d157f5b5f82cb0ce74
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-17 19:27:02 +0800

    feat : define index method

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataJadwal;
+use App\Models\NotarisDetail;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class JadwalController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = auth()->user();
+
+        $notarisDetail = NotarisDetail::where('user_id', $user->user_id)->first();
+
+        $dataJadwal = DataJadwal::where('notaris_id', $notarisDetail->notaris_id)->get(['id_jadwal','notaris_id','tanggal','waktu_mulai','waktu_selesai']);
+
+        return Inertia::render('Notaris/Jadwal/Index', [
+            'dataJadwal' => $dataJadwal,
+        ]);
+    }
 }
