a32a8ca572e1866ebe7f7403d6d7f572f2e1ae09
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-17 17:51:39 +0800

    feat : implement layout

--- resources/js/Pages/Notaris/Dashboard.vue
+++ resources/js/Pages/Notaris/Dashboard.vue
@@ -1,10 +1,18 @@
 <script setup>
+// import core api
+
+// import layouts
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+
+// variables, functions
+const pageTitle = 'Dashboard'
+
 </script>
 
 <template>
-    <div>
-    </div>
+    <AuthenticatedLayout :pageTitle="pageTitle">
+        <template #pageContent>
+            <span class="text-2xl">Hello Notaris</span>
+        </template>
+    </AuthenticatedLayout>
 </template>
-
-<style scoped>
-</style>
