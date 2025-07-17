649d3c4a6a5108f48154e9da19e349097b617181
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-17 19:27:48 +0800

    feat : create file and implement data jadwal

---
+++ resources/js/Pages/Notaris/Jadwal/List.vue
@@ -0,0 +1,78 @@
+<script setup>
+// import core api
+import { onBeforeMount, ref } from 'vue'
+import { FilterMatchMode } from '@primevue/core/api'
+
+
+// lifecycle hooks
+onBeforeMount(() =>
+{
+    dataJadwalClone.value = props.dataJadwal
+})
+
+// variables, functions
+const props = defineProps({
+    dataJadwal : Object
+})
+
+const emit = defineEmits(['refreshPage', 'editData'])
+
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+})
+
+const dataJadwalClone = ref([])
+
+const editData = dataId =>
+{
+    emit('editData', dataId)
+}
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <!-- dataTable -->
+        <DataTable :value="dataJadwalClone" dataKey="index" class="shadow border border-blue-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable :filters="filters">
+            <template #header>
+                <div class="flex flex-col gap-y-2">
+                    <!-- Basic Filter -->
+                    <div class="flex justify-between items-center gap-x-2">
+                        <IconField class="w-full">
+                            <InputIcon>
+                                <i class="pi pi-search me-4" />
+                            </InputIcon>
+                            <InputText v-model="filters['global'].value" placeholder="Cari Data Jadwal" size="small" fluid/>
+                        </IconField>
+                    </div>
+                </div>
+            </template>
+            <template #footer>
+                <span>Jumlah Data Jadwal ({{ dataJadwalClone.length }})</span>
+            </template>
+            <template #loading>
+                <span class="flex justify-center">Sedang Memuat...</span>
+            </template>
+            <template #empty>
+                <span class="flex justify-center">Tidak Ada Data Jadwal</span>
+            </template>
+            <Column header="No" field="nomor"/>
+            <Column header="Tanggal" field="tanggal"/>
+            <Column header="Waktu Mulai" field="waktu_mulai"/>
+            <Column header="Waktu Selesai" field="waktu_selesai"/>
+            <Column header="Alasan" field="alasan">
+                <template #body="{data}">
+                     <span :class="{'text-gray-400' : !data.alasan}">
+                        {{ data.alasan ? data.alasan : 'Tidak ada kendala' }}
+                    </span>
+                </template>
+            </Column>
+            <Column header="Action" frozen align-frozen="right" class="w-1 whitespace-nowrap">
+                <template #body="{data}">
+                    <div class="flex items-center gap-2">
+                        <Button @click="editData(data.id_berkas)" severity="info" size="small" icon="pi pi-pen-to-square"/>
+                    </div>
+                </template>
+            </Column>
+        </DataTable>
+    </div>
+</template>
