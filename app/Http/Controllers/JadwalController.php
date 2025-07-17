5568c5cc5d216e1abff410dd3be222d120ec5708
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-17 20:26:04 +0800

    feat : bind repositroy, fix index method and define store method

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -2,23 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DataJadwalRequest;
 use App\Models\DataJadwal;
 use App\Models\NotarisDetail;
-use Illuminate\Http\Request;
+use App\Repositories\Interfaces\DataJadwalRepositoryInterface;
+use App\Services\RedirectWithNotification;
 use Inertia\Inertia;
 
 class JadwalController extends Controller
 {
+    // repository
+    protected $repository;
+
+    public function __construct(DataJadwalRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
         $user = auth()->user();
 
-        $notarisDetail = NotarisDetail::where('user_id', $user->user_id)->first();
+        $notarisDetails = NotarisDetail::where('user_id', $user->user_id)->first();
 
-        $dataJadwal = DataJadwal::where('notaris_id', $notarisDetail->notaris_id)->get(['id_jadwal','notaris_id','tanggal','waktu_mulai','waktu_selesai']);
+        $dataJadwal = DataJadwal::where('notaris_id', $notarisDetails->notaris_id)->get(['id_jadwal','notaris_id','tanggal','waktu_mulai','waktu_selesai']);
 
         return Inertia::render('Notaris/Jadwal/Index', [
             'dataJadwal' => $dataJadwal,
         ]);
+    }
+
+    public function store(DataJadwalRequest $req)
+    {
+        $data = $req->validated();
+        $create = $this->repository->create($data);
+
+        return RedirectWithNotification::back(
+            $create,
+            'Berhasil menambahkan jadwal : ' . $req['tanggal'],
+            'Gagal menambahkan jadwal : ' . $req['tanggal'],
+        );
     }
 }
