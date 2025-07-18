c68247d6f14b3497ae6dafdb4153249497ffd2af
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-19 01:06:58 +0800

    feat : create file and implement dataJadwal

---
+++ resources/js/Pages/Klien/Jadwal/Index.vue
@@ -0,0 +1,73 @@
+<script setup>
+// import core api
+import { computed, defineAsyncComponent, ref } from 'vue'
+
+// import store / composables
+
+// import layout, components
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import LoadingSpinner from '@/Components/LoadingSpinner.vue'
+
+// lifecycle hooks
+
+// variables, functions
+const props = defineProps({
+    dataJadwal : Object
+})
+
+const pageTitle = ref('Daftar Jadwal')
+
+const currentTab = ref('List')
+
+const switchComponents = (component,title) =>
+{
+    currentTab.value = component
+    pageTitle.value = title
+}
+
+// async component
+const loadComponent = componentName =>
+{
+    return  defineAsyncComponent({
+        loader : () => import(`./${componentName}.vue`),
+        loadingComponent : LoadingSpinner,
+        delay : 200,
+        timeout: 3000
+    })
+}
+
+const currentComponent = computed(() => {
+  return loadComponent(currentTab.value)
+})
+
+const componentProps = computed(() => {
+    switch (currentTab.value) {
+
+        case 'List':
+        return {
+            dataJadwal : props.dataJadwal?.map((p, i) => ({ nomor: i + 1, ...p })),
+        };
+
+        default:
+        return {};
+    }
+})
+</script>
+
+<template>
+    <AuthenticatedLayout :pageTitle="pageTitle">
+        <template #pageContent>
+            <!-- tabs -->
+            <div class="flex gap-x-4">
+                <Button @click="switchComponents('List','Daftar Jadwal')" label="Daftar Jadwal" :severity="currentTab==='List'?'primary':'secondary'" icon="pi pi-list" v-if="currentTab==='List'"/>
+            </div>
+            <!-- components -->
+            <div class="flex flex-col mt-4">
+                <component :is="currentComponent" v-bind="componentProps"/>
+            </div>
+        </template>
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+</style>
