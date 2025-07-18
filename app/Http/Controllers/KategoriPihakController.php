898b33f8d6bcfd74340aafb7c82c9d47069895e8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-18 21:25:51 +0800

    feat : bind related repository

--- app/Http/Controllers/KategoriPihakController.php
+++ app/Http/Controllers/KategoriPihakController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\KategoriPihak;
+use App\Repositories\Interfaces\KategoriPihakRepositoryInterface;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class KategoriPihakController extends Controller
 {
+    // repository
+    protected $repository;
+
+    public function __construct(KategoriPihakRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
         $dataKtgPihak = KategoriPihak::select('id_kategori_pihak', 'nama_kategori_pihak')->get();
