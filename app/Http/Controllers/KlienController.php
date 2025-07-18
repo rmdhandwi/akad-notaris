7392a8e9d668be253f8d1e0c92969b3bb75032ee
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-19 02:02:23 +0800

    feat : define indexSyaratJenisLayanan method

--- app/Http/Controllers/KlienController.php
+++ app/Http/Controllers/KlienController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\DataJadwal;
+use App\Models\JenisLayanan;
 use Inertia\Inertia;
 
 class KlienController extends Controller
@@ -20,6 +21,18 @@ class KlienController extends Controller
 
         return Inertia::render('Klien/Jadwal/Index', [
             'dataJadwal' => $dataJadwal,
+        ]);
+    }
+
+    public function indexSyaratJenisLayanan()
+    {
+        $dataJenisLayanan = JenisLayanan::select('id_jenis','id_kategori','nama_jenis')->with([
+            'kategoriPihak:id_kategori_pihak,id_jenis,nama_kategori_pihak',
+            'kategoriPihak.dataBerkas:id_berkas,id_kategori_pihak,nama_berkas',
+        ])->get();
+
+        return Inertia::render('Klien/JenisLayanan/Index', [
+            'dataJenisLayanan' => $dataJenisLayanan,
         ]);
     }
 }
