76b9b108ca810218d600d778a2bdefc62b0bfac7
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-18 21:04:51 +0800

    feat : add kategori pihak menu

--- resources/js/Composables/sidebarMenu.js
+++ resources/js/Composables/sidebarMenu.js
@@ -2,6 +2,7 @@ export const adminMenu = [
     { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
     { label: "Kategori Layanan", icon: "pi pi-briefcase", route: "admin.layanan.kategori.index"},
     { label: "Jenis Layanan", icon: "pi pi-briefcase", route: "admin.layanan.jenis.index"},
+    { label: "Kategori Pihak", icon: "pi pi-users", route: "admin.layanan.pihak.index"},
     { label: "Data Berkas", icon: "pi pi-book", route: "admin.berkas.index"},
     { label: "Data Staf", icon: "pi pi-user", route: "admin.users.staf.index"},
     { label: "Data Notaris", icon: "pi pi-user", route: "admin.users.notaris.index"},
