ac01d08dd4bd866468f5078e7d9628887c445073
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-19 01:06:32 +0800

    feat : define indexJadwal method

--- app/Http/Controllers/KlienController.php
+++ app/Http/Controllers/KlienController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\DataJadwal;
 use Inertia\Inertia;
 
 class KlienController extends Controller
@@ -10,5 +10,16 @@ class KlienController extends Controller
     public function dashboard()
     {
         return Inertia::render('Klien/Dashboard');
+    }
+
+    public function indexJadwal()
+    {
+        $dataJadwal = DataJadwal::where('alasan', NULL)->select('id_jadwal', 'notaris_id', 'tanggal', 'waktu_mulai', 'waktu_selesai', 'alasan')->with([
+            'notaris:notaris_id,nomor_jabatan_notaris,nama_notaris'
+        ])->get();
+
+        return Inertia::render('Klien/Jadwal/Index', [
+            'dataJadwal' => $dataJadwal,
+        ]);
     }
 }
