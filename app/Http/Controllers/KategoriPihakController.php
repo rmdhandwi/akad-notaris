4d608e4f0c9763b01f484a7d4efd3ba62305b3c4
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-18 21:21:53 +0800

    feat : define index method

--- app/Http/Controllers/KategoriPihakController.php
+++ app/Http/Controllers/KategoriPihakController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriPihak;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class KategoriPihakController extends Controller
 {
-    //
+    public function index()
+    {
+        $dataKtgPihak = KategoriPihak::select('id_kategori_pihak', 'nama_kategori_pihak')->get();
+        return Inertia::render('Admin/KategoriPihak/Index', [
+            'dataKtgPihak' => $dataKtgPihak,
+        ]);
+    }
 }
