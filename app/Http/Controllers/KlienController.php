898a4a94fab0518234ec744aaf7af682736e0f2c
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 03:44:49 +0800

    feat : bind repository, add storeDataPihak method and redirectToFormBerkasPihak

--- app/Http/Controllers/KlienController.php
+++ app/Http/Controllers/KlienController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DataPihakRequest;
 use App\Models\DataJadwal;
 use App\Models\JenisLayanan;
 use App\Models\KategoriLayanan;
+use App\Models\PermintaanLayanan;
+use App\Repositories\Interfaces\DataPihakRepositoryInterface;
+use App\Services\RedirectWithNotification;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class KlienController extends Controller
 {
+    // repository
+    protected $repository;
+
+    public function __construct(DataPihakRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function dashboard()
     {
         return Inertia::render('Klien/Dashboard');
@@ -35,6 +48,35 @@ class KlienController extends Controller
 
         return Inertia::render('Klien/JenisLayanan/Index', [
             'dataKategoriLayanan' => $dataKategoriLayanan,
+        ]);
+    }
+
+    public function storeDataPihak(DataPihakRequest  $req)
+    {
+        $user = auth()->user();
+
+        $data = $req->validated();
+
+        $store = $this->repository->storeDataPihak($user->user_id, $data);
+
+        return redirect()->route('klien.layanan.permintaan.form')->with([
+            'id_permintaan' => $store,
+            'notif_status' => $store ? 'success' : 'error',
+            'notif_message' => $store ? 'Berhasil menyimpan data pihak' : 'Gagal menyimpan data pihak ',
+            'notif_duration' => 5000,
+        ]);
+    }
+
+    public function redirectToFormBerkasPihak(Request $id_permintaan)
+    {
+        $dataPermintaan = PermintaanLayanan::select('id_permintaan','id_jenis','id_pihak','id_jadwal','tgl_permintaan')->with([
+            'jenisLayanan:id_jenis,nama_jenis',
+            'jenisLayanan.kategoriPihak:id_kategori_pihak,id_jenis,nama_kategori_pihak',
+            'jenisLayanan.kategoriPihak.dataBerkas:id_berkas,id_kategori_pihak,nama_berkas',
+        ])->where('id_permintaan', $id_permintaan)->firstOrFail();
+
+        return Inertia::render('Klien/JenisLayanan/FormPermintaan', [
+            'dataPermintaan' => $dataPermintaan,
         ]);
     }
 }
