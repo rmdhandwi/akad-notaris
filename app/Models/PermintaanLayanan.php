1dba9099be920d2b8632e1fd68d85966ea65bde6
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 03:42:40 +0800

    feat : define table, columns and relations

--- app/Models/PermintaanLayanan.php
+++ app/Models/PermintaanLayanan.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PermintaanLayanan extends Model
 {
-    //
+    use HasFactory;
+
+    protected
+        $table = 'permintaan_layanan',
+        $primaryKey = 'id_permintaan',
+        $fillable = [
+            'id_jenis',
+            'id_pihak',
+            'id_staf',
+            'id_jadwal',
+            'tgl_permintaan',
+            'status',
+        ];
+
+    public function pihak()
+    {
+        return $this->belongsTo(DataPihak::class, 'id_pihak', 'id_pihak');
+    }
+
+    public function jenisLayanan()
+    {
+        return $this->belongsTo(JenisLayanan::class, 'id_jenis', 'id_jenis');
+    }
 }
