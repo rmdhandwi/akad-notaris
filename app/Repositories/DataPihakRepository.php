4a1181c1fea6df5c1ff691ad328b8d0ba98dee14
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 03:41:38 +0800

    feat : implement method storeDataPihak

--- app/Repositories/DataPihakRepository.php
+++ app/Repositories/DataPihakRepository.php
@@ -3,10 +3,50 @@
 namespace App\Repositories;
 
 use App\Models\DataPihak;
+use App\Models\PermintaanLayanan;
 use App\Repositories\Interfaces\DataPihakRepositoryInterface;
+use Illuminate\Support\Facades\DB;
 
 class DataPihakRepository implements DataPihakRepositoryInterface
 {
+    public function storeDataPihak(int $userIdPihak1, array $data)
+    {
+        return DB::transaction(function () use ($data, $userIdPihak1) {
+            // Pihak pertama
+            $pihak1 = new DataPihak([
+                'user_id' => $userIdPihak1,
+                'id_kategori_pihak' => $data['pihak'][0]['id_kategori_pihak'],
+                'nama_pihak' => $data['pihak'][0]['nama_pihak'],
+                'nik_pihak' => $data['pihak'][0]['nik_pihak'],
+                'no_hp_pihak' => $data['pihak'][0]['no_hp_pihak'],
+                'alamat_pihak' => $data['pihak'][0]['alamat_pihak'],
+                'tipe_pihak' => $data['pihak'][0]['tipe_pihak'],
+            ]);
+            $pihak1->save();
+
+            // Pihak kedua
+            $pihak2 = new DataPihak([
+                'user_id' => null,
+                'id_pihak_terkait' => $pihak1->id_pihak,
+                'id_kategori_pihak' => $data['pihak'][1]['id_kategori_pihak'],
+                'nama_pihak' => $data['pihak'][1]['nama_pihak'],
+                'nik_pihak' => $data['pihak'][1]['nik_pihak'],
+                'no_hp_pihak' => $data['pihak'][1]['no_hp_pihak'],
+                'alamat_pihak' => $data['pihak'][1]['alamat_pihak'],
+                'tipe_pihak' => $data['pihak'][1]['tipe_pihak'],
+            ]);
+            $pihak2->save();
+
+            // Simpan permintaan
+            $permintaan = PermintaanLayanan::create([
+                'id_jenis' => $data['id_jenis'],
+                'id_pihak' => $pihak1->id_pihak,
+            ]);
+
+            return $permintaan->id_permintaan;
+        });
+    }
+
     public function create(array $data): bool
     {
         $store = DataPihak::create($data);
