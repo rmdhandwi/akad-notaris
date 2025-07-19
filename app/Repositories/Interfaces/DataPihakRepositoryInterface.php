dae34256520d2af874cabc1f4e3be98fd55e5e58
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 03:41:26 +0800

    feat : add method storeDataPihak

--- app/Repositories/Interfaces/DataPihakRepositoryInterface.php
+++ app/Repositories/Interfaces/DataPihakRepositoryInterface.php
@@ -5,6 +5,7 @@ namespace App\Repositories\Interfaces;
 interface DataPihakRepositoryInterface
 {
     public function create(array $data);
+    public function storeDataPihak(int $userIdPihak1, array $data);
     public function update($id, array $data);
     public function delete($id);
 }
