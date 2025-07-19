fe5d1da1dc0e7baae045f4d399ebceb7f76137cd
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 03:42:27 +0800

    feat : define table, columns and relations

--- app/Models/DataPihak.php
+++ app/Models/DataPihak.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DataPihak extends Model
 {
-    //
+    use HasFactory;
+
+    protected
+        $table = 'data_pihak',
+        $primaryKey = 'id_pihak',
+        $fillable = [
+            'user_id',
+            'id_pihak_terkait',
+            'id_kategori_pihak',
+            'nama_pihak',
+            'nik_pihak',
+            'no_hp_pihak',
+            'alamat_pihak',
+            'tipe_pihak',
+        ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function kategoriPihak()
+    {
+        return $this->belongsTo(KategoriPihak::class, 'id_kategori_pihak', 'id_kategori_pihak');
+    }
+
+    public function pihakTerkait()
+    {
+        return $this->belongsTo(DataPihak::class, 'id_pihak_terkait', 'id_pihak_terkait');
+    }
 }
