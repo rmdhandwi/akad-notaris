e4966de03d7f44afe9182a2f1b758ee77131938a
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 22:53:44 +0800

    feat : add route to uploadBerkas

--- resources/js/Pages/Klien/Permintaan/List.vue
+++ resources/js/Pages/Klien/Permintaan/List.vue
@@ -2,6 +2,8 @@
 // import core api
 import { onBeforeMount, ref } from 'vue'
 import { FilterMatchMode } from '@primevue/core/api'
+import { useForm } from '@inertiajs/vue3'
+import { useConfirm } from 'primevue'
 
 // import store / composables
 import { useFormatTanggal } from '@/Composables/useFormatTanggal'
@@ -24,6 +26,7 @@ onBeforeMount(() =>
 const props = defineProps({
     dataPermintaan : Object
 })
+const confirm = useConfirm()
 
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
@@ -32,6 +35,34 @@ const filters = ref({
 const dataPermintaanClone = ref([])
 
 const { formatToDMY } = useFormatTanggal()
+
+const form = useForm({
+    id_permintaan : null,
+})
+
+const uploadBerkas = (id_permintaan) =>
+{
+    form.id_permintaan = id_permintaan
+
+    console.log(form.id_permintaan)
+
+    confirm.require({
+        message: `Lanjut ke upload berkas ?`,
+        header: 'Peringatan',
+        icon: 'pi pi-exclamation-triangle',
+        rejectProps: {
+            label: 'Batal',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptProps: {
+            label: `Lanjut`,
+        },
+        accept: () => {
+            form.post(route('klien.layanan.permintaan.simpan-id'))
+        }
+    })
+}
 </script>
 
 <template>
@@ -61,6 +92,11 @@ const { formatToDMY } = useFormatTanggal()
             <Column header="Tanggal Permintaan" field="permintaan.tgl_permintaan"/>
             <Column header="Jenis Layanan" field="permintaan.jenis_layanan.nama_jenis"/>
             <Column header="Status" field="permintaan.status"/>
+            <Column header="Action" frozen align-frozen="right">
+                <template #body="{data}">
+                    <Button @click="uploadBerkas(data.permintaan.id_permintaan)" outlined icon="pi pi-file" label="Upload berkas" size="small" v-if="!data.permintaan.id_jadwal"/>
+                </template>
+            </Column>
         </DataTable>
     </div>
 </template>
