b95c439486fe0117e32185ce74a9a312071d64c8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-20 21:42:24 +0800

    feat : add daftarPermintaan method and catch id_permintaan in redirectToFormBerkas method

--- app/Http/Controllers/KlienController.php
+++ app/Http/Controllers/KlienController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\DataPihakRequest;
 use App\Models\DataJadwal;
+use App\Models\DataPihak;
 use App\Models\JenisLayanan;
 use App\Models\KategoriLayanan;
 use App\Models\PermintaanLayanan;
 use App\Repositories\Interfaces\DataPihakRepositoryInterface;
 use App\Services\RedirectWithNotification;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -51,6 +53,20 @@ class KlienController extends Controller
         ]);
     }
 
+    public function daftarPermintaan()
+    {
+        $user = auth()->user();
+
+        $dataPermintaan = DataPihak::where('user_id', $user->user_id)->select('id_pihak')->with([
+            'permintaan:id_permintaan,id_pihak,id_jenis,tgl_permintaan,status',
+            'permintaan.jenisLayanan:id_jenis,nama_jenis',
+        ])->get();
+
+        return Inertia::render('Klien/Permintaan/Index', [
+            'dataPermintaan' => $dataPermintaan,
+        ]);
+    }
+
     public function storeDataPihak(DataPihakRequest  $req)
     {
         $user = auth()->user();
@@ -61,22 +77,36 @@ class KlienController extends Controller
 
         return redirect()->route('klien.layanan.permintaan.form')->with([
             'id_permintaan' => $store,
-            'notif_status' => $store ? 'success' : 'error',
-            'notif_message' => $store ? 'Berhasil menyimpan data pihak' : 'Gagal menyimpan data pihak ',
+            'notif_status' => 'success',
+            'notif_message' => 'Berhasil menyimpan data pihak',
             'notif_duration' => 5000,
         ]);
     }
 
-    public function redirectToFormBerkasPihak(Request $id_permintaan)
+    public function redirectToFormBerkasPihak()
     {
+        $id_permintaan = session('id_permintaan');
+
         $dataPermintaan = PermintaanLayanan::select('id_permintaan','id_jenis','id_pihak','id_jadwal','tgl_permintaan')->with([
             'jenisLayanan:id_jenis,nama_jenis',
             'jenisLayanan.kategoriPihak:id_kategori_pihak,id_jenis,nama_kategori_pihak',
             'jenisLayanan.kategoriPihak.dataBerkas:id_berkas,id_kategori_pihak,nama_berkas',
         ])->where('id_permintaan', $id_permintaan)->firstOrFail();
 
+        $dataJadwal = DataJadwal::with([
+            'notaris:notaris_id,nama_notaris'
+        ])->doesntHave('permintaan')
+        ->select('id_jadwal', 'notaris_id', 'tanggal', 'waktu_mulai', 'waktu_selesai', 'alasan')->get();
+
+        $dataJadwal = $dataJadwal->map(function ($jadwal) {
+            $jadwal->waktu_mulai = Carbon::createFromFormat('H:i:s', $jadwal->waktu_mulai)->format('H:i');
+            $jadwal->waktu_selesai = Carbon::createFromFormat('H:i:s', $jadwal->waktu_selesai)->format('H:i');
+            return $jadwal;
+        });
+
         return Inertia::render('Klien/JenisLayanan/FormPermintaan', [
             'dataPermintaan' => $dataPermintaan,
+            'dataJadwal' => $dataJadwal,
         ]);
     }
 }
