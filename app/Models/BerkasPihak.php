2340b4c5d31fb00f5ea913a54b1c8b13f0f2a8af
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-21 19:06:57 +0800

    feat : create file and define table columns

---
+++ app/Models/BerkasPihak.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BerkasPihak extends Model
+{
+    use HasFactory;
+
+    protected $table = 'berkas_pihak';
+    protected $primaryKey = 'id_berkas_pihak';
+
+    protected $fillable = [
+        'id_permintaan',
+        'id_berkas',
+        'id_pihak',
+        'berkas_path',
+    ];
+}
