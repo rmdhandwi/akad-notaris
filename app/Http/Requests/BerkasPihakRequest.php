0c4746b40fa033826efa78145f8cd00e356b6dad
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-21 20:37:40 +0800

    feat : fix file validation attributes, rules and messages

--- app/Http/Requests/BerkasPihakRequest.php
+++ app/Http/Requests/BerkasPihakRequest.php
@@ -17,9 +17,8 @@ class BerkasPihakRequest extends FormRequest
         return [
             'id_jadwal' => 'Jadwal layanan',
             'id_jenis' => 'Jenis layanan',
-            'berkas' => 'required|array',
             'berkas.*.*.id_pihak' => 'Pihak',
-            'berkas.*.*.file' => 'File ',
+            'berkas.*.*.file' => 'Berkas',
             'berkas.*.*.id_berkas' => 'Jenis berkas',
             'berkas.*.*.nama_berkas' => 'Nama berkas',
         ];
@@ -29,21 +28,31 @@ class BerkasPihakRequest extends FormRequest
     {
         return [
             'id_jenis' => 'required|exists:jenis_layanan,id_jenis',
-            'id_permintaan' => 'required|exists:permintaan,id_permintaan',
-            'berkas.*.*.id_pihak' => 'required|exists:data_pihak,id_pihak',
-            'berkas.*.*.nama_berkas' => 'required|string|max:255',
+            'id_jadwal' => 'required|exists:data_jadwal,id_jadwal',
+            'id_permintaan' => 'required|exists:permintaan_layanan,id_permintaan',
+            'berkas' => 'required|array',
+            'berkas.*' => 'required|array',
             'berkas.*.*.file' => 'required|file|mimes:pdf,jpg,jpeg,png|max:2048',
+            'berkas.*.*.id_pihak' => 'required|exists:data_pihak,id_pihak',
+            'berkas.*.*.id_berkas' => 'required|exists:data_berkas,id_berkas',
+            'berkas.*.*.nama_berkas' => 'required|string|max:255',
         ];
     }
 
     public function messages(): array
     {
         return [
+            'id_jadwal.required' => ':attribute wajib dipilih.',
             '*.required' => ':attribute wajib diisi.',
             '*.unique'   => ':attribute sudah terdaftar.',
             '*.exists'   => ':attribute tidak terdaftar.',
+            'berkas.*.required' => ':attribute wajib diisi.',
             'berkas.*.*.required' => ':attribute wajib diisi.',
             'berkas.*.*.exists'   => ':attribute tidak terdaftar.',
+            'berkas.*.*.file.required' => ':attribute wajib diupload.',
+            'berkas.*.*.file.file' => ':attribute harus berupa file.',
+            'berkas.*.*.file.mimes' => ':attribute harus berupa file bertipe: :values.',
+            'berkas.*.*.file.max' => ':attribute maksimal berukuran :max kilobyte.',
         ];
     }
 }
